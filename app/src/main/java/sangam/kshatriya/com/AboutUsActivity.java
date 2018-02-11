d1ca13def635da53e84cb877929f20a717c8edce
Umadevi Samudrala <umadevi.samu@gmail.com>
2018-02-11 06:18:05 -0600

    Added about us content

---
+++ app/src/main/java/sangam/kshatriya/com/AboutUsActivity.java
@@ -0,0 +1,14 @@
+package sangam.kshatriya.com;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AboutUsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+}
---
+++ app/src/main/java/sangam/kshatriya/com/StoryActivity.java
@@ -0,0 +1,14 @@
+package sangam.kshatriya.com;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class StoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+    }
+
+}
